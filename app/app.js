f9a63ab17c4d03135eb41f341a21b7f6db68af5f
张彧豪 <1198826114@qq.com>
2023-03-21 17:58:07 +0800

    fix: asd

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"
 import typeMapping from "./utils/typeMapping";
 function App({ pageType }) {
   useEffect(() => {
