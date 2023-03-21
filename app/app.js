1d48fe05bad5010acaa4172161feaf3006770b2b
张彧豪 <1198826114@qq.com>
2023-03-21 18:38:15 +0800

    fix: 这么意识

--- app/app.js
+++ app/app.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import typeMapping from "./utils/typeMapping";
 function App({ pageType }) {
   useEffect(() => {
     if (typeof pageType === "number") {
-      typeMapping["ocs1"  ]().then((value) => {
+      typeMapping["ocs1"]().then((value) => {
         setPage(value.default);
-      }); 
+      });
     }
-  }, [pageType]) ; 
+  }, [pageType]);
   const [page, setPage] = useState("");
   return <div>app4{page}</div>;
 }
 
-export default App;  
+export default App;
